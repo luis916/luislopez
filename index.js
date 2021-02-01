645f1b070c3c9dbc1d34b573a59bf11c84eb69f6
luis916 <69111765+luis916@users.noreply.github.com>
2021-02-01 05:11:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+//let nombre = prompt("Cuál es tu nombre?");
+//if (nombre == "mercedes" || nombre == "fernanda" || nombre == "mercedes ortega clavijo")
+//{
+  //  document.write(`<h1 style="color:red;top:-600px;position: relative;">Bienvenida bb</h1>`);
+//}else {document.write(`<h1 style="color:red;top:-570px;position: relative;">Bienvenid@: ${nombre}</h1>`);}
 let nombre = prompt("Cuál es tu nombre?");
-if (nombre == "mercedes" || nombre == "fernanda" || nombre == "mercedes ortega clavijo")
+
+
+
+while (nombre != "luis" )
 {
-    document.write(`<h1 style="color:red;top:-600px;position: relative;">Bienvenida bb</h1>`);
-}else {document.write(`<h1 style="color:red;top:-570px;position: relative;">Bienvenid@: ${nombre}</h1>`);}
+    prompt("Cuál es tu nombre?");
+}
