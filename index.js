698d5a17ddb814d35ff28cfb7684efb118831f29
luis916 <69111765+luis916@users.noreply.github.com>
2021-02-01 04:31:49 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Cuál es tu nombre?");
+if (nombre == "mercedes" || nombre == "fernanda" || nombre == "mercedes ortega clavijo")
+{
+    document.write(`<h1 style="color:red;top:-600px;position: relative;">Bienvenida bb</h1>`);
+}else {document.write(`<h1 style="color:red;top:-570px;position: relative;">Bienvenid@: ${nombre}</h1>`);}
